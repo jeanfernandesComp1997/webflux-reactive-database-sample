0767b044f7793cf7bc82fa21103a75ce6bfaf6bb
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2023-10-02 23:28:00 -0300

    refactor: data setup

--- src/main/kotlin/com/sample/webfluxreactivedatabase/infrastructure/data/configuration/DataSetupConfiguration.kt
+++ src/main/kotlin/com/sample/webfluxreactivedatabase/infrastructure/data/configuration/DataSetupConfiguration.kt
@@ -1,15 +1,15 @@
-package com.sample.webfluxreactivedatabase.application.services.impl
+package com.sample.webfluxreactivedatabase.infrastructure.data.configuration
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.Resource
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
-import org.springframework.stereotype.Service
 import org.springframework.util.StreamUtils
 import java.nio.charset.StandardCharsets
 
-@Service
-class DataSetupService(
+@Configuration
+class DataSetupConfiguration(
     @Value("classpath:h2/init.sql") private val initSql: Resource,
     private val entityTemplate: R2dbcEntityTemplate
 ) : CommandLineRunner {
